4cac820e5ec599c4af2d93a3c8586f38cee104b2
吴欣谕 <14461859+Wu-xinyu-Siri@user.noreply.gitee.com>
2026-04-23 20:23:00 +0800

    save current workspace to uav

--- decision_task/start_decision.sh
+++
@@ -1,3 +0,0 @@
-export AMENT_PREFIX_PATH=/home/nvidia/NJU-algorithm/decision_task/install/decision:$AMENT_PREFIX_PATH
-source ./install/setup.sh
-ros2 launch decision pose_init_and_decision.launch.py
